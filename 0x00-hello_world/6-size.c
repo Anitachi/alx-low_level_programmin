b51b923cf2e5dd2f18e8720a6227d645496dd6d3
Anitachi <chikaanita750@gmail.com>
2022-04-08 22:17:27 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Print the size of various types
+ *Return: should be 0
+ */
+int main(void)
+{
+       printf("%d\n", sizeof(char));
+       printf("%d\n", sizeof(int));
+       Printf("%d\n", sizeof(float));
+       print("%d\n", sizeof(double));
+       return (0)
+}
