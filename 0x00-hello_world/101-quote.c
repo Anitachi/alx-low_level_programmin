8ca7e28fc48fd920c1943d7c38639b23fc6cdfbe
Anitachi <chikaanita750@gmail.com>
2022-04-09 03:49:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * return: answer should be 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
