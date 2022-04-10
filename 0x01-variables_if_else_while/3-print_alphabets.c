bf4ab122b12f7cc745348531efc2496d490a025d
Anitachi <chikaanita750@gmail.com>
2022-04-10 16:20:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- print all alphabet in lower case
+ * print all alphabet in upper case
+ * Return: is always 0
+ */
+
+int main(void)
+{
+	char c;
+	char b;
+
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+		putchar('\n');
+	}
+
+	{
+		for (b = 'A'; B <= 'Z'; b++)
+			putchar(b);
+	}
+	return (0)
+}
+
