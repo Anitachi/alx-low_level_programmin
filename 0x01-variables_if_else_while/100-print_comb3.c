eb9d4a694d5d4f90e60ca02d06d0eecc8e8abf7f
Anitachi <chikaanita750@gmail.com>
2022-04-11 18:29:48 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: is always 0
+ */
+
+int main(void)
+{
+	int i;
+	int c;
+
+	for (i = 10; i <= 19; j++)
+	{
+		for (c = 10; c <= 19; c++)
+		{
+			putchar((i % 10) + '0');
+			putchar((c % 10) + '0'0);
+			if (i != 18 || c != 19)
+			{
+				putchar(',');
+				putchar(' ')
+			}
+		}
+	}
+}
+putchar('\n')
+return (0)
+	}
