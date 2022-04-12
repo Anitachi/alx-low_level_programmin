77b577f231ae9786716896ce6f195726d80c2644
Anitachi <chikaanita750@gmail.com>
2022-04-11 18:55:32 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int m;
+	int p;
+
+	for  (i = 0; i <= 9; i++)
+	{
+		for (m = 0; m <= 9; k++)
+		{
+			for (p = 0; p <= 9; k++)
+			{
+				if (p > m && m > i)
+				{
+					putchar(i + '0');
+					putchar(m + '0');
+					putchar(p + '0');
+					if (i != 7 || m != 8 || p != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
