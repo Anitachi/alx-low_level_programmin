3f4d77574c823df8a7b6feb109fda89cf8e74bfa
Anitachi <chikaanita750@gmail.com>
2022-04-12 20:48:25 -0700

    omo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,11 @@
 #define MAIN_H_INCLUDED
-
-
-
-int _putchat(char c);
+int _putchar(char c);
 
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
 int _islower(int c);
-
 int _isalpha(int c);
 
 int print_sign(int n);
