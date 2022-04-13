f07d182197c23fb9be96457adfdfbf20d85e5c27
Anitachi <chikaanita750@gmail.com>
2022-04-13 04:28:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print in lower case
+ * Return : is always 0
+ */
+
+void print_alphabet(void);
+{
+	int c;
+
+	for  (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('c');
+
+	}
+	_putchar('\n');
+	return (0)
+}
