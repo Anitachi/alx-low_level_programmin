7f5be765b2b8839493daaf511d46d5814d498da2
Anitachi <chikaanita750@gmail.com>
2022-04-13 09:07:06 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,19 +7,19 @@
 
 int print_last_digit(int n)
 {
-	int r = 0;
+	int r = '0';
 
 	if (r < 0)
 	{
 
 		r = -1 * (n % 10);
-		_putchar(r + 0);
+		_putchar(r + '0');
 		return (r);
 	}
 	else
 	{
 		r = n % 10;
-		_putchar(r + 0);
+		_putchar(r + '0');
 		return (r);
 	}
 }
