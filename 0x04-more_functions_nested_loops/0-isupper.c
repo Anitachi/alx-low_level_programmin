46bfed1e85931e26addfe963b67942cf5debebf1
Anitachi <chikaanita750@gmail.com>
2022-04-14 11:50:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: assign a value
+ * Return: if it is uppercase return 1 else 0
+ */
+
+int _isupper(int c)
+{
+	for (c >= 'A'; c <= 'Z')
+	{
+		return  (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is really getting tough but we not giving up
+More functions, more nested loops
