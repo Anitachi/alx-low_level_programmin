7160b951392d5f53424d8d36aa623dd1445cfefb
Anitachi <chikaanita750@gmail.com>
2022-04-15 14:56:19 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal -Entry point
+ * @n: assign a value
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
