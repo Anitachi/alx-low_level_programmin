82d932315e23421bf9ed096dab9bed1db633dd5e
Anitachi <chikaanita750@gmail.com>
2022-04-14 17:03:59 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,23 +5,21 @@
  * main- assign a random num to the variable of n
  * Return: value is always 0
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive/n" n);
+		printf("%d is positive/n" i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 	
 	return (0);
